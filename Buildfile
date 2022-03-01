41ee7ff6074341bd6499619b33888b9a58c631db
Shion <57133270+Ehnryu@users.noreply.github.com>
2022-03-01 04:42:49 +0000

    clean up + read me

--- Buildfile
+++ Buildfile
@@ -7,5 +7,8 @@ rm -rf taskel.egg-info
 @q python -m pip install --upgrade build
 @q python -m pip install --upgrade twine
 @q python -m build
+\endfunc
+
+@function upload:
 python3 -m twine upload --repository pypi dist/*
 \endfunc
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
 setup(name='taskel',
-      version='0.0.1',
+      version='0.0.2',
       description='The python library for all types of tasks',
+      long_description="Readme.md",
       url='http://github.com/Ehnryu/taskel',
       author='Ehnryu/Sakurai07',
       author_email='blzzardst0rm@gmail.com',
